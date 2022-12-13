ff3dd00b07106b292bc3b7de2ab55cc743a46eb6
yohanhl <yohannl030@gmail.com>
2022-12-13 20:55:47 +0700

    Konfigurasi file config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/klinik-wp2/';
+$config['base_url'] = 'http://localhost:8080/klinik-wp2/';
 
 /*
 |--------------------------------------------------------------------------
