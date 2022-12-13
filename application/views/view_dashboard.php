00b502e28fb5a3af1d4a0ff9d50931a6ab8b600a
Dehansovyan <dehansovyan@gmail.com>
2022-12-13 21:05:31 +0700

    Menambahkan view dashboard

---
+++ application/views/view_dashboard.php
@@ -0,0 +1,21 @@
+
+<section class="content">
+  <div class="container-fluid mt-4">
+    <div class="jumbotron">
+      <h1 class="display-4">Aplikasi Klinik Codeigniter3</h1>
+      <p class="lead">Aplikasi untuk tugas akhir mata kuliah Web Programming 2</p>
+      <hr class="my-4">
+      <h2>Nama Kelompok</h2>
+      <ul>
+        <li>Aryo</li>
+        <li>Dehan</li>
+        <li>Faisal</li>
+        <li>Hafif</li>
+        <li>Yohannes</li>
+      </ul>
+    </div>
+  </div>
+
+</section>
+
+    
